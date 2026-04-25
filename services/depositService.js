235abe3e57e7ea6889e22363d6d48e857542456f
bjornpetersson1 <bjornpetersson1@live.se>
2026-04-25 21:30:17 +0200

    another bug

--- services/depositService.js
+++ services/depositService.js
@@ -1,5 +1,6 @@
 import { GetValueFromComponent } from "./valueService.js";
 import { gameState } from "../data/stateData.js";
+import { ShowInfoScreen } from "../presentation/components/infoRender.js";
 export const CalculatorLogic = (expressionContainer, valueSource, button) => {
   const key = button.dataset.key;
   if (key === "C") {
@@ -14,5 +15,9 @@ export const CalculatorLogic = (expressionContainer, valueSource, button) => {
 };
 export const HandleStringToNumTransfer = (valueOriginId) => {
   const value = Math.round(Number(GetValueFromComponent(valueOriginId)));
-  gameState.userState.balance += value;
+  if (isNaN(value)) {
+    ShowInfoScreen("You tried to pass not a number");
+  } else {
+    gameState.userState.balance += value;
+  }
 };
